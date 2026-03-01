3083a3022ead3e39532626d2c2e1ed7fea4b0866
Denis Papizhuk <ipz244_pdr@student.ztu.edu.ua>
2026-03-01 22:53:07 +0200

    додано 3 завдання лаб2

---
+++ ЛАБ2/3Task/3Task/ConsoleApp1/Authenticator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public sealed class Authenticator
+    {
+        private static Authenticator _instance;
+        private static readonly object _lock = new object();
+        private Authenticator()
+        {
+            Console.WriteLine("Authenticator: Об'єкт створено (єдиний раз).");
+        }
+        public static Authenticator GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Authenticator();
+                    }
+                }
+            }
+            return _instance;
+        }
+        public void AuthenticateUser(string username)
+        {
+            Console.WriteLine($"[AUTH]: Користувач {username} успішно пройшов перевірку.");
+        }
+    }
+}
---
+++ ЛАБ2/3Task/3Task/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("=== Перевірка роботи Одинака ===");
+
+            Authenticator auth1 = Authenticator.GetInstance();
+            auth1.AuthenticateUser("Admin_1");
+
+            Authenticator auth2 = Authenticator.GetInstance();
+            auth2.AuthenticateUser("Admin_2");
+
+            if (ReferenceEquals(auth1, auth2))
+            {
+                Console.WriteLine("\nРЕЗУЛЬТАТ: auth1 та auth2 — це один і той самий екземпляр.");
+                Console.WriteLine("Принцип Singleton дотримано.");
+            }
+            else
+            {
+                Console.WriteLine("\nПОМИЛКА: Створено різні екземпляри!");
+            }
+
+            Console.WriteLine("\nНатисніть будь-яку клавішу для виходу...");
+            Console.ReadKey();
+        }
+    }
+}
