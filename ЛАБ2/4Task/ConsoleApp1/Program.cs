701fabea50b5979d277e9dccb4dda44da2c2b4bb
Denis Papizhuk <ipz244_pdr@student.ztu.edu.ua>
2026-03-01 22:59:14 +0200

    Додано 4 завдання

---
+++ ЛАБ2/4Task/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Virus grandParent = new Virus(10.5, 50, "Alfa-Virus", "Root");
+
+            Virus parent1 = new Virus(5.2, 20, "Beta-1", "Infector");
+            Virus parent2 = new Virus(4.8, 18, "Beta-2", "Trojan");
+            grandParent.Children.Add(parent1);
+            grandParent.Children.Add(parent2);
+
+            parent1.Children.Add(new Virus(1.1, 2, "Gamma-Sub1", "Small"));
+            parent1.Children.Add(new Virus(0.9, 1, "Gamma-Sub2", "Small"));
+
+            Console.WriteLine("--- Оригінальна сім'я вірусів ---");
+            grandParent.ShowInfo();
+
+            Virus clonedFamily = (Virus)grandParent.Clone();
+
+            Console.WriteLine("\n--- Клонована сім'я вірусів ---");
+            clonedFamily.ShowInfo();
+
+            Console.WriteLine("\n--- Перевірка глибокого копіювання ---");
+            bool isSameReference = ReferenceEquals(grandParent, clonedFamily);
+            bool isFirstChildSame = ReferenceEquals(grandParent.Children[0], clonedFamily.Children[0]);
+
+            Console.WriteLine($"Батьки - це різні об'єкти в пам'яті? {!isSameReference}");
+            Console.WriteLine($"Діти - це різні об'єкти в пам'яті? {!isFirstChildSame}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ЛАБ2/4Task/ConsoleApp1/Virus.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class Virus : ICloneable
+    {
+        public double Weight { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public List<Virus> Children { get; set; }
+
+        public Virus(double weight, int age, string name, string type)
+        {
+            Weight = weight;
+            Age = age;
+            Name = name;
+            Type = type;
+            Children = new List<Virus>();
+        }
+
+        public object Clone()
+        {
+            Virus clone = new Virus(this.Weight, this.Age, this.Name, this.Type);
+
+            foreach (var child in this.Children)
+            {
+                clone.Children.Add((Virus)child.Clone());
+            }
+
+            return clone;
+        }
+
+        public void ShowInfo(int indent = 0)
+        {
+            string space = new string(' ', indent * 4);
+            Console.WriteLine($"{space}Вірус: {Name} (Тип: {Type}, Вік: {Age}, Вага: {Weight})");
+            foreach (var child in Children)
+            {
+                child.ShowInfo(indent + 1);
+            }
+        }
+    }
+}
